442de86ba3d7bc4bc722a22a4d06ad389c85b38e
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-09-22 16:44:49 -0300

    conditionals

--- spring-boot-bootcamp-resources-main/02. model-view-controller/conditionals/starter/conditionals/src/main/java/com/ltp/conditionals/ConditionalsController.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/conditionals/starter/conditionals/src/main/java/com/ltp/conditionals/ConditionalsController.java
@@ -1,15 +1,17 @@
 package com.ltp.conditionals;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class ConditionalsController {
 
-    @GetMapping(value="/")
-    public String getMethodName() {
+    @GetMapping(value = "/")
+    public String getMethodName(Model model) {
+        model.addAttribute("sales", 30);
+        model.addAttribute("product", "chair");
         return "conditionals";
     }
-    
+
 }
--- spring-boot-bootcamp-resources-main/02. model-view-controller/workbook 2.3/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/workbook 2.3/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -1,15 +1,16 @@
 package com.ltp.workbook;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class WorkbookController {
 
-    @GetMapping(value="/")
-    public String getSign() {
+    @GetMapping(value = "/")
+    public String getSign(Model model) {
+        model.addAttribute("speed", 65);
         return "sign";
     }
-    
+
 }
