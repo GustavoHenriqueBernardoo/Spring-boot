5a62e6d8b97d7f67d5d32246214c235d3d33eeb1
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-09-27 12:12:28 -0300

    thymeleaf loops

--- spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,5 +1,10 @@
 package com.ltp.gradesubmission;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +12,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GradeController {
 
+  List<Grade> studentGrades = Arrays.asList(
+      new Grade("John Doe", "Potions", "-C"),
+      new Grade("Ronaldo", "Potions", "-A"),
+      new Grade("Harry", "Defense Against the Dark Arts", "+A")
+
+  );
+
   @GetMapping("/grades")
   public String getGrades(Model model) {
-    Grade grade = new Grade("John Doe", "Potions", "-C");
-    model.addAttribute("grade", grade);
+
+    model.addAttribute("grades", studentGrades);
     return "grades";
   }
 }
--- spring-boot-bootcamp-resources-main/02. model-view-controller/workbook 2.8/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/workbook 2.8/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WorkbookController {
-    @GetMapping(value="/")
+    @GetMapping(value = "/")
     public String getLandingPage() {
         return "page";
     }
-    
+
 }
