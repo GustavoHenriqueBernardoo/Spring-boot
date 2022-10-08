e5d83a709f9543bdeaad61e5e8f980e11adc270f
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-10-08 17:24:31 -0300

    change CRUD operations to a repository file

--- spring-boot-bootcamp-resources-main/04. three-layer-codebase/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/controller/GradeController.java
+++ spring-boot-bootcamp-resources-main/04. three-layer-codebase/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/controller/GradeController.java
@@ -1,4 +1,4 @@
-package com.ltp.gradesubmission;
+package com.ltp.gradesubmission.controller;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,40 +12,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ltp.gradesubmission.Constants;
+import com.ltp.gradesubmission.Grade;
+import com.ltp.gradesubmission.repository.GradeRepository;
+
 @Controller
 public class GradeController {
 
-    List<Grade> studentGrades = new ArrayList<>();
+    GradeRepository gradeRepository = new GradeRepository();
 
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id) {
         int index = getGradeIndex(id);
-        model.addAttribute("grade", index == Constants.NOT_FOUND ? new Grade() : studentGrades.get(index));
+        model.addAttribute("grade", index == Constants.NOT_FOUND ? new Grade() : gradeRepository.getGrade(index));
         return "form";
     }
 
     @PostMapping("/handleSubmit")
     public String submitForm(@Valid Grade grade, BindingResult result) {
-        if (result.hasErrors()) return "form";
+        if (result.hasErrors())
+            return "form";
 
         int index = getGradeIndex(grade.getId());
         if (index == Constants.NOT_FOUND) {
-            studentGrades.add(grade);
+            gradeRepository.addGrade(grade);
         } else {
-            studentGrades.set(index, grade);
+            gradeRepository.updateGrade(grade, index);
         }
         return "redirect:/grades";
     }
 
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        model.addAttribute("grades", studentGrades);
+        model.addAttribute("grades", gradeRepository.getGrades());
         return "grades";
     }
 
     public int getGradeIndex(String id) {
-        for (int i = 0; i < studentGrades.size(); i++) {
-            if (studentGrades.get(i).getId().equals(id)) return i;
+        for (int i = 0; i < gradeRepository.getGrades().size(); i++) {
+            if (gradeRepository.getGrades().get(i).getId().equals(id))
+                return i;
         }
         return Constants.NOT_FOUND;
     }
---
+++ spring-boot-bootcamp-resources-main/04. three-layer-codebase/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/service/GradeService.java
@@ -0,0 +1,5 @@
+package com.ltp.gradesubmission.repository.service;
+
+public class GradeService {
+
+}
