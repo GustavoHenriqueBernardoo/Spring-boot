8cc21c055c1b1a57975407620441847d025bda74
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-10-10 11:10:45 -0300

    three layer codebase

--- spring-boot-bootcamp-resources-main/04. three-layer-codebase/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/controller/GradeController.java
+++ spring-boot-bootcamp-resources-main/04. three-layer-codebase/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/controller/GradeController.java
@@ -14,46 +14,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ltp.gradesubmission.Constants;
 import com.ltp.gradesubmission.Grade;
-import com.ltp.gradesubmission.repository.GradeRepository;
+import com.ltp.gradesubmission.repository.service.GradeService;
 
 @Controller
 public class GradeController {
 
-    GradeRepository gradeRepository = new GradeRepository();
+    GradeService gradeService = new GradeService();
 
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id) {
-        int index = getGradeIndex(id);
-        model.addAttribute("grade", index == Constants.NOT_FOUND ? new Grade() : gradeRepository.getGrade(index));
+        model.addAttribute("grade", gradeService.getGradeById(id));
         return "form";
     }
 
     @PostMapping("/handleSubmit")
     public String submitForm(@Valid Grade grade, BindingResult result) {
-        if (result.hasErrors())
+        if (result.hasErrors()) {
             return "form";
-
-        int index = getGradeIndex(grade.getId());
-        if (index == Constants.NOT_FOUND) {
-            gradeRepository.addGrade(grade);
-        } else {
-            gradeRepository.updateGrade(grade, index);
         }
+
+        gradeService.submitGrade(grade);
+
         return "redirect:/grades";
     }
 
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        model.addAttribute("grades", gradeRepository.getGrades());
+        model.addAttribute("grades", gradeService.getGrades());
         return "grades";
-    }
-
-    public int getGradeIndex(String id) {
-        for (int i = 0; i < gradeRepository.getGrades().size(); i++) {
-            if (gradeRepository.getGrades().get(i).getId().equals(id))
-                return i;
-        }
-        return Constants.NOT_FOUND;
     }
 
 }
