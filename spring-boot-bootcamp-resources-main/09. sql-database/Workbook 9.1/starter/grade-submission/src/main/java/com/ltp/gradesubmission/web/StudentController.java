d1bb08d2c805098310b71f3343674f5bac96a112
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-10-20 16:59:42 -0300

    sql and spring-boot

--- spring-boot-bootcamp-resources-main/09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ spring-boot-bootcamp-resources-main/09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -1,11 +1,21 @@
 package com.ltp.gradesubmission.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ltp.gradesubmission.entity.Student;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping(value = "/student")
 public class StudentController {
 
+  @GetMapping("/student")
+  public ResponseEntity<Student> getStudent(@PathVariable Long id) {
+
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
 }
