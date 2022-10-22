ad015ee59a3f07e17115c6c1f649821437a33499
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-10-22 11:55:59 -0300

    add get, post and delete to CourseController

--- spring-boot-bootcamp-resources-main/09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/GradeController.java
+++ spring-boot-bootcamp-resources-main/09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/GradeController.java
@@ -14,7 +14,6 @@ import com.ltp.gradesubmission.entity.Grade;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/grade")
