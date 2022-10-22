e9c3559c268b56314b1ba52b76b45ba0b7920521
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-10-22 11:45:22 -0300

    add rest controller: student and grade

--- spring-boot-bootcamp-resources-main/09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ spring-boot-bootcamp-resources-main/09. sql-database/Workbook 9.1/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/student")
@@ -28,7 +27,7 @@ public class StudentController {
   @PostMapping("")
   public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
 
-    return new ResponseEntity<>(HttpStatus.CREATED);
+    return new ResponseEntity<>(student, HttpStatus.CREATED);
   }
 
   @DeleteMapping("/{id}")
