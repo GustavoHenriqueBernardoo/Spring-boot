4ee98f23caa950a81dc16cc6d885f0bdf3c9f44a
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-10-27 11:21:41 -0300

    spring jpa

--- spring-boot-bootcamp-resources-main/09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ spring-boot-bootcamp-resources-main/09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -2,34 +2,32 @@ package com.ltp.gradesubmission.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.*;
+
+@Entity
+@Table(name = "student")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getBirthDate() {
-        return this.birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
 
 }
--- spring-boot-bootcamp-resources-main/09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
+++ spring-boot-bootcamp-resources-main/09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
@@ -2,28 +2,36 @@ package com.ltp.gradesubmission.service;
 
 import java.util.List;
 
-import com.ltp.gradesubmission.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.repository.StudentRepository;
+
+@Service
 public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
 
     @Override
     public Student getStudent(Long id) {
-        return null;
+        return studentRepository.findById(id).get();
     }
 
     @Override
     public Student saveStudent(Student student) {
-        return null;
+        return studentRepository.save(student);
     }
 
     @Override
-    public void deleteStudent(Long id) {        
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
     }
 
     @Override
     public List<Student> getStudents() {
-        return null;
+        return (List<Student>) studentRepository.findAll();
     }
-
 
 }
